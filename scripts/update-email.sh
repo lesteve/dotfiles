75a9f574604f2bd12d05b28e6532632cf7190d72
Loïc Estève <loic.esteve@ymail.com>
2022-06-24 15:53:59 +0200

    Fix update-email.sh script

--- scripts/update-email.sh
+++ scripts/update-email.sh
@@ -8,7 +8,7 @@ mbsync -a -V > /tmp/mbsync.log 2>&1
 # https://github.com/djcb/mu/issues/8#issuecomment-396649525
 if pgrep -f 'mu server'; then
     echo "mu is already running, going through emacs" >> /tmp/mbsync.log
-    emacsclient -e '(mu4e-maildirs-extension-force-update)' >> /tmp/mbsync.log 2>&1
+    emacsclient -e '(mu4e-update-index)' >> /tmp/mbsync.log 2>&1
 else
     echo "mu is not running, indexing mail database" >> /tmp/mbsync.log
     mu index --maildir=~/.mail >> /tmp/mbsync.log 2>&1
